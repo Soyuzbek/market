2cdaf37821303403b952ce5b4847d54b0dfa905c
soyuzbek <soyuzbek196.kg@gmail.com>
2019-07-20 17:37:26 +0600

    almost done

--- products/templatetags/ispk.py
+++ products/templatetags/ispk.py
@@ -6,7 +6,7 @@ register = template.Library()
 @register.filter
 def ispk(data):
     try:
-        if int(data) != 0:
+        if len(str(data)) > 0:
             return True
     except (TypeError, ValueError):
         pass
