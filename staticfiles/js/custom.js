3dfdb2aca39784f496c7de0063a66e986807b4a5
soyuzbek <soyuzbek196.kg@gmail.com>
2019-07-24 08:55:11 +0000

    translted to ky

--- staticfiles/js/custom.js
+++ staticfiles/js/custom.js
@@ -43,9 +43,8 @@ $('#cartDelete').click(function () {
             $('#total').html('0 som');
             parent = $('body');
             if (data['status'])
-                if($('#myalert').length <= 0) parent.append('<small id="myalert" class="alert alert-success" style="z-index: 999;">' + data['data'] + '</small>');
-            else
-               if($('#myalert').length <= 0) parent.append('<small id="myalert" class="alert alert-danger" style="z-index: 999;">' + data['data'] + '</small>');
+                if ($('#myalert').length <= 0) parent.append('<small id="myalert" class="alert alert-success" style="z-index: 999;">' + data['data'] + '</small>');
+                else if ($('#myalert').length <= 0) parent.append('<small id="myalert" class="alert alert-danger" style="z-index: 999;">' + data['data'] + '</small>');
             setTimeout(function () {
                 $('#myalert').hide('slow', function () {
                     $('#myalert').remove();
@@ -70,9 +69,8 @@ function cartAdd(id, url, token, qty) {
         success: function (data) {
             parent = $('body');
             if (data['status'])
-                if($('#myalert').length <= 0) parent.append('<small id="myalert" class="alert alert-success" style="z-index: 999;">' + data['data'] + '</small>');
-            else
-               if($('#myalert').length <= 0) parent.append('<small id="myalert" class="alert alert-danger" style="z-index: 999;">' + data['data'] + '</small>');
+                if ($('#myalert').length <= 0) parent.append('<small id="myalert" class="alert alert-success" style="z-index: 999;">' + data['data'] + '</small>');
+                else if ($('#myalert').length <= 0) parent.append('<small id="myalert" class="alert alert-danger" style="z-index: 999;">' + data['data'] + '</small>');
             setTimeout(function () {
                 $('#myalert').hide('slow', function () {
                     $('#myalert').remove();
@@ -94,9 +92,8 @@ function favAdd(url, slug, token) {
         success: function (data) {
             parent = $('body');
             if (data['status'])
-                if($('#myalert').length <= 0) parent.append('<small id="myalert" class="alert alert-success" style="z-index: 999;">' + data['data'] + '</small>');
-            else
-               if($('#myalert').length <= 0) parent.append('<small id="myalert" class="alert alert-danger" style="z-index: 999;">' + data['data'] + '</small>');
+                if ($('#myalert').length <= 0) parent.append('<small id="myalert" class="alert alert-success" style="z-index: 999;">' + data['data'] + '</small>');
+                else if ($('#myalert').length <= 0) parent.append('<small id="myalert" class="alert alert-danger" style="z-index: 999;">' + data['data'] + '</small>');
             setTimeout(function () {
                 $('#myalert').hide('slow', function () {
                     $('#myalert').remove();
@@ -133,9 +130,215 @@ jQuery(document).ready(function ($) {
 
 });
 
-
-$('.open-modal').click(function(e)
-{
+$('.open-modal').click(function (e) {
     e.preventDefault();
     $('#mymodal').modal('show');
+});
+$(document).ready(function () {
+    jQuery.validator.addMethod(
+        "regex",
+        function (value, element, regexp) {
+            alert('inside add method');
+            if (regexp.test(value)) {
+                console.log(regexp);
+                console.log(value);
+                console.log(element);
+                console.log(true);
+
+                return true;
+            } else {
+                console.log(regexp);
+                console.log(value);
+                console.log(element);
+                console.log(false)
+                return false
+
+            }
+        },
+        "Please check your input."
+    );
+    // validation of the subscribe form
+    $('form[id="email-form"]').validate({
+        rules: {
+            email: {
+                required: true,
+                email: true,
+            }
+        },
+        messages: {
+            email: "<small class='invalid feedback text-white'> Enter a valid email </small>",
+        },
+        submitHandler: function (form) {
+            $.ajax({
+                url: $(form).attr('action'),
+                method: 'post',
+                datatype: 'json',
+                data: {
+                    email: $('#subs-email').val(),
+                    csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val()
+                },
+                success: function (data) {
+                    msg = 'Your review added successfully';
+                    $('body').append('<small id="myalert" class="alert alert-success">' + msg + '</small>');
+                    setTimeout(function () {
+                        $('#myalert').hide('slow', function () {
+                            $('#myalert').remove();
+                        });
+                    }, 2500);
+
+                },
+                error: function (data) {
+                    msg = 'This email is already exists!';
+                    $('body').append('<small id="myalert" class="alert alert-danger">' + msg + '</small>');
+                    setTimeout(function () {
+                        $('#myalert').hide('slow', function () {
+                            $('#myalert').remove();
+                        });
+                    }, 2500);
+                }
+            });
+            return false;
+        }
+    });
+    // validation of the review form
+    $('#review-form').validate({
+        rules: {
+            name: {
+                required: true,
+                minlength: 3
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            rating: {
+                required: true,
+            },
+            comment: {
+                required: false,
+                minlength: 25
+            }
+        },
+        messages: {
+            name: {
+                required: "<small class='invalid feedback text-danger'> Please enter your name </small>",
+                minlength: "<small class='invalid feedback text-danger'> The name can not be short than 3 character </small>"
+            },
+            email: {
+                required: "<small class='invalid feedback text-danger'> The email field can not be empty </small>",
+                email: "<small class='invalid feedback text-danger'> Please enter a valid email </small>"
+            },
+            rating: "<small class='invalid feedback text-danger'> Please rate us </small>",
+            comment: "<small class='invalid feedback text-danger'> Comment must contain at least 25 character"
+        },
+        submitHandler: function (form) {
+            let $form = $(form),
+                url = $form.attr('action');
+
+            /* Send the data using post with element id name and name2*/
+            let request = $.ajax({
+                url: url,
+                method: 'POST',
+                dataType: 'json',
+                data: {
+                    name: $('#id_name').val(),
+                    email: $('#id_email').val(),
+                    rating: $('#id_rating').val(),
+                    comment: $('#id_comment').val(),
+                    product: $('#id_product').val(),
+                    csrfmiddlewaretoken: $('input[name="csrfmiddlewaretoken"]').val()
+                },
+            });
+            request.done(function (msg) {
+                $('#mymodal').modal('hide');
+                parent = $('body');
+                msg = 'Your review added successfully';
+                parent.append('<small id="myalert" class="alert alert-success">' + msg + '</small>');
+                setTimeout(function () {
+                    $('#myalert').hide('slow', function () {
+                        $('#myalert').remove();
+                    });
+                }, 2500);
+            });
+            request.fail(function (jqXHR, status) {
+                let temp = JSON.parse(jqXHR['responseText']);
+                console.log(temp);
+                parent = $form;
+                if (temp != null)
+                    for (let i = 0; i < temp.email.length; i++) {
+                        parent.append('<small id="myalert" class="alert alert-danger">' + temp.email[i] + '</small>');
+                        setTimeout(function () {
+                            $('#myalert').hide('slow', function () {
+                                $('#myalert').remove();
+                            });
+                        }, 2500);
+                    }
+
+            });
+        }
+    });
+    // Checkout form validation
+    $('#checkout-form').validate({
+        rule: {
+            first_name: {
+                required: true,
+                minlength: 3
+            },
+            last_name: {
+                required: true,
+                minlength: 4
+            },
+            email: {
+                required: true,
+                email: true,
+                maxlength: 254
+            },
+            address: {
+                required: true,
+                minlength: 8,
+                maxlength: 254
+            },
+            phone: {
+                required: true,
+                minlength: 12,
+                regex: "\+996?\d{10,15}",
+                digits: true
+            },
+            comment: {
+                required: false,
+                minlength: 25
+            }
+        },
+        messages: {
+            first_name: {
+                required: "<small class='invalid feedback text-danger'> Please enter your name </small>",
+                minlength: "<small class='invalid feedback text-danger'> The name can't be short than 3 character </small>"
+            },
+            last_name: {
+                required: "<small class='invalid feedback text-danger'> Last name can't be blank </small>",
+                minlength: "<small class='invalid feedback text-danger'> The last name can't be short than 4 character </small>"
+            },
+            email: {
+                required: "<small class='invalid feedback text-danger'> Enter your email </small>",
+                email: "<small class='invalid feedback text-danger'> Please enter a valid email </small>",
+                maxlength: "<small class='invalid feedback text-danger'> Enter shorter email </small>"
+            },
+            address: {
+                required: "<small class='invalid feedback text-danger'> We can't take order without address </small>",
+                minlength: "<small class='invalid feedback text-danger'> The address can't be short than 8 digits </small>",
+                maxlength: "<small class='invalid feedback text-danger'> Enter shorter address </small>"
+            },
+            phone: {
+                required: "<small class='invalid feedback text-danger'> Enter your phone number </small>",
+                minlength: "<small class='invalid feedback text-danger'> Phone can't be shorter than 12 digits </small>",
+                regex: "<small class='invalid feedback text-danger'> You just entered invalid phone format </small>"
+            },
+            comment: {
+                minlength: "<small class='invalid feedback text-danger'> Comment must contain at least 25 character </small>"
+            }
+        },
+        submitHandler: function (form) {
+            form.submit();
+        }
+    })
 });
